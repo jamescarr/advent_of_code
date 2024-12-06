55bad51535c91b335215c47a1cddad355db6668b
James Carr <james.r.carr@gmail.com>
2024-12-06 07:18:46 -0600

    slight refactoring of solution for Day 1

--- lib/advent_of_code/Y2024/day1.ex
+++ lib/advent_of_code/Y2024/day1.ex
@@ -2,7 +2,7 @@ defmodule AdventOfCode.Y2024.Day1 do
   @behaviour AdventOfCode.SolutionBehaviour
 
   def solve do
-    {list1, list2} = AdventOfCode.Input.ensure_exists!(2024, 1)
+    {left, right} = AdventOfCode.Input.ensure_exists!(2024, 1)
     |> File.stream!()
     |> Enum.map(&String.trim/1)
     |> Enum.map(fn line ->
@@ -11,27 +11,22 @@ defmodule AdventOfCode.Y2024.Day1 do
     end)
     |> Enum.unzip
 
-    IO.puts("Distance between both lists: #{find_distance(list1, list2)}")
-    IO.puts("Score of both lists: #{similarity_score(list1, list2)}")
+    IO.puts("Distance between both lists: #{find_distance(left, right)}")
+    IO.puts("Score of both lists: #{similarity_score(left, right)}")
   end
 
-  def find_distance(list1, list2) do
-    sorted_list1 = list1 |> Enum.sort
-    sorted_list2 = list2 |> Enum.sort
-
-    Enum.zip(sorted_list1, sorted_list2)
+  def find_distance(left, right) do
+    Enum.zip(Enum.sort(left), Enum.sort(right))
     |> Enum.map(fn {x, y} -> abs(x - y) end)
     |> Enum.sum
   end
 
-  def similarity_score(list1, list2) do
-    frequencies = Enum.frequencies(list2)
-    result = Enum.map(list1, fn num ->
+  def similarity_score(left, right) do
+    frequencies = Enum.frequencies(right)
+    Enum.map(left, fn num ->
       num * Map.get(frequencies, num, 0)
     end)
     |> Enum.sum
-
-    result
   end
 
 end
